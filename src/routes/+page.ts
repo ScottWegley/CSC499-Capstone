cddb1f2afc46cdfa1fec7939be2802d6bf68f1cf
scottwegley <scottwegley@gmail.com>
2024-02-10 18:35:16 -0800

    created a function to fetch clouldflare trace info and store it

---
+++ src/routes/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+
+const ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/;
+
+/* Inspired by the work done here https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/ */
+export const load: PageLoad = async ({ fetch, params }) => {
+    let item ="";
+    await fetch("https://www.cloudflare.com/cdn-cgi/trace").then(res => res.text()).then(data => item = data);
+    item = item.match(ipRegex)![0];
+	return { item };
+};
