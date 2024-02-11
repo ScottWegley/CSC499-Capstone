adb6d22f77b0ad5b70ff157074a1442266768469
scottwegley <scottwegley@gmail.com>
2024-02-10 23:10:02 -0800

    add icon and site title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import '../app.pcss';
-	import Navbar from '$lib/components/navbar.svelte';
+	import {Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
-<Navbar />
+<Navbar fluid>
+	<NavBrand href="/">
+		<img src="$lib/imgs/Lock_Icon.svg" style="height: 35px; width: 35px">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white" style="margin-left: 7px;">Cybersecurity and You</span>
+	</NavBrand>
+</Navbar>
 
 <slot />
