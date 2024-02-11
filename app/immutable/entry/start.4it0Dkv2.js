66592ff432f22d099c698d6718e1363cde9814dd
scottwegley <scottwegley@gmail.com>
2024-02-11 00:03:42 -0800

    Updates

---
+++ app/immutable/entry/start.4it0Dkv2.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.EtCmKRIH.js";export{t as start};
---
+++ app/immutable/nodes/3.98CXoWc6.js
@@ -0,0 +1,25 @@
+import{s as S}from"../chunks/scheduler.EIbqhEqt.js";import{S as T,i as D,e as u,s as g,c as y,a as w,g as j,b,d as q,f as $,h as m,j as P,k as d,m as v,l as z,t as _,n as x,o as C,p as I,q as k}from"../chunks/index.-DnIZj1S.js";import{H as A,P as L}from"../chunks/P.H73u9U9i.js";function B(c){let e;return{c(){e=I("About")},l(n){e=k(n,"About")},m(n,i){d(n,e,i)},d(n){n&&m(e)}}}function E(c){let e;return{c(){e=I(`This website was created as my capstone project for my Bachelors Degree. It was made during my\r
+		final undergraduate semester at California Lutheran University in Spring of 2024. I developed it\r
+		under the advisement of Dr. Chang-Shyh Peng, the Computer Science department chair at the time,\r
+		with inspiration from Dr. Craig Reinhart, the former chair and one of my recurring professors. I\r
+		decided to create a webapp primarily because my experience with web development is incredibly\r
+		limited and informal. I plan to end up in Cybersecurity for my career, and that requires being\r
+		familiar with a number of different environments from mobile to desktop to web. This seemed the\r
+		perfect oppurtunity to expose myself to an environment I was less experienced in. I additionally\r
+		decided to make the theme and subject of the website Cybersecurity, with a focus on\r
+		Cryptography. Categorizing and collating information that I feel I know has proven an excellent\r
+		way for me to verify my own knowledge, and this seemed like a great chance to do that. This page\r
+		hosts a number of the documents I created during the development process, all of which you are\r
+		welcome to and encouraged to view.`)},l(n){e=k(n,`This website was created as my capstone project for my Bachelors Degree. It was made during my\r
+		final undergraduate semester at California Lutheran University in Spring of 2024. I developed it\r
+		under the advisement of Dr. Chang-Shyh Peng, the Computer Science department chair at the time,\r
+		with inspiration from Dr. Craig Reinhart, the former chair and one of my recurring professors. I\r
+		decided to create a webapp primarily because my experience with web development is incredibly\r
+		limited and informal. I plan to end up in Cybersecurity for my career, and that requires being\r
+		familiar with a number of different environments from mobile to desktop to web. This seemed the\r
+		perfect oppurtunity to expose myself to an environment I was less experienced in. I additionally\r
+		decided to make the theme and subject of the website Cybersecurity, with a focus on\r
+		Cryptography. Categorizing and collating information that I feel I know has proven an excellent\r
+		way for me to verify my own knowledge, and this seemed like a great chance to do that. This page\r
+		hosts a number of the documents I created during the development process, all of which you are\r
+		welcome to and encouraged to view.`)},m(n,i){d(n,e,i)},d(n){n&&m(e)}}}function H(c){let e,n="About",i,a,o,h,s,l;return o=new A({props:{tag:"h2",class:"mb-4",$$slots:{default:[B]},$$scope:{ctx:c}}}),s=new L({props:{class:"mb-6 text-lg dark:text-gray-400 sm:px-16",align:"center",size:"sm",$$slots:{default:[E]},$$scope:{ctx:c}}}),{c(){e=u("title"),e.textContent=n,i=g(),a=u("div"),y(o.$$.fragment),h=g(),y(s.$$.fragment),this.h()},l(t){e=w(t,"TITLE",{"data-svelte-h":!0}),j(e)!=="svelte-1xr0egx"&&(e.textContent=n),i=b(t),a=w(t,"DIV",{class:!0});var r=q(a);$(o.$$.fragment,r),h=b(r),$(s.$$.fragment,r),r.forEach(m),this.h()},h(){P(a,"class","text-center")},m(t,r){d(t,e,r),d(t,i,r),d(t,a,r),v(o,a,null),z(a,h),v(s,a,null),l=!0},p(t,[r]){const f={};r&1&&(f.$$scope={dirty:r,ctx:t}),o.$set(f);const p={};r&1&&(p.$$scope={dirty:r,ctx:t}),s.$set(p)},i(t){l||(_(o.$$.fragment,t),_(s.$$.fragment,t),l=!0)},o(t){x(o.$$.fragment,t),x(s.$$.fragment,t),l=!1},d(t){t&&(m(e),m(i),m(a)),C(o),C(s)}}}class F extends T{constructor(e){super(),D(this,e,null,H,S,{})}}export{F as component};
