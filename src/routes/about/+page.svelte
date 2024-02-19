52861b66d4ff9a49fddc3b457681108966a2e31e
scottwegley <scottwegley@gmail.com>
2024-02-19 01:43:23 -0800

    limited text width for about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
-	import { Heading, P } from "flowbite-svelte";
-
+	import { Heading, P } from 'flowbite-svelte';
 </script>
 
 <title>About</title>
 
 <div class="text-center">
 	<Heading tag="h2" class="mb-4">About</Heading>
-	<P class="mb-6 text-lg dark:text-gray-400 sm:px-16" align="center" size="sm">
-		This website was created as my capstone project for my Bachelors Degree. It was made during my
-		final undergraduate semester at California Lutheran University in Spring of 2024. I developed it
-		under the advisement of Dr. Chang-Shyh Peng, the Computer Science department chair at the time,
-		with inspiration from Dr. Craig Reinhart, the former chair and one of my recurring professors. I
-		decided to create a webapp primarily because my experience with web development is incredibly
-		limited and informal. I plan to end up in Cybersecurity for my career, and that requires being
-		familiar with a number of different environments from mobile to desktop to web. This seemed the
-		perfect oppurtunity to expose myself to an environment I was less experienced in. I additionally
-		decided to make the theme and subject of the website Cybersecurity, with a focus on
-		Cryptography. Categorizing and collating information that I feel I know has proven an excellent
-		way for me to verify my own knowledge, and this seemed like a great chance to do that. This page
-		hosts a number of the documents I created during the development process, all of which you are
-		welcome to and encouraged to view.
-	</P>
+	<div class="mx-auto max-w-screen-2xl">
+		<P class="mb-6 text-lg dark:text-gray-400 sm:px-16" align="center" size="sm">
+			This website was created as my capstone project for my Bachelors Degree. It was made during my
+			final undergraduate semester at California Lutheran University in Spring of 2024. I developed
+			it under the advisement of Dr. Chang-Shyh Peng, the Computer Science department chair at the
+			time, with inspiration from Dr. Craig Reinhart, the former chair and one of my recurring
+			professors. I decided to create a webapp primarily because my experience with web development
+			is incredibly limited and informal. I plan to end up in Cybersecurity for my career, and that
+			requires being familiar with a number of different environments from mobile to desktop to web.
+			This seemed the perfect oppurtunity to expose myself to an environment I was less experienced
+			in. I additionally decided to make the theme and subject of the website Cybersecurity, with a
+			focus on Cryptography. Categorizing and collating information that I feel I know has proven an
+			excellent way for me to verify my own knowledge, and this seemed like a great chance to do
+			that. This page hosts a number of the documents I created during the development process, all
+			of which you are welcome to and encouraged to view.
+		</P>
+	</div>
 </div>
 
 <style>
