8cebca11dc375cd2ebd89cd0db955d37fb7dd957
scottwegley <scottwegley@gmail.com>
2024-02-19 01:43:31 -0800

    verbage change on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,10 @@
 <div class="text-center">
 	<Heading tag="h2" class="mb-4">Cybersecurity and You!</Heading>
 	<P class="mb-6 text-lg dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48" align="center">
-		Welcome to my Cybersecurity website! While I'm by no means an expert on the subject, this site
+		Welcome to my Cybersecurity website! While I'm by no means an expert, this site
 		does serve as my personal collection of knowledge and tools related to my exploration of the
 		subject. Don't expect anything fancy, but once your expectations have been lowered there are
-		definitely some interesting tools here, most of which concern Cryptography!
+		definitely some interesting tools here, most related to Cryptography!
 	</P>
 </div>
 
