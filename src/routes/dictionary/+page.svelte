a8ea389bace683253a4c942b4d97a2665ede123c
scottwegley <scottwegley@gmail.com>
2024-02-19 01:43:59 -0800

    created local storage that pulls from default word list text are reflects word lists changes persist

--- src/routes/dictionary/+page.svelte
+++ src/routes/dictionary/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { Heading, P, Button, Fileupload, Textarea } from 'flowbite-svelte';
+	import { writable } from 'svelte/store';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	let fileuploadprops = {
 		id: 'user_avatar'
@@ -12,6 +15,40 @@
 		rows: 40,
 		placeholder: 'The Wordlist'
 	};
+
+	/** This variable will store our wordlist.  If one exists in storage, use that.  If not, default to our basic word list. */
+	export const wordlist = writable(browser && (localStorage.getItem('wordlist') || 'empty'));
+	/** When wordlist changes, update the session storage representation. */
+	wordlist.subscribe((val) => browser && localStorage.setItem('wordlist', val.toString()));
+
+	/** This function loads the basic word list we provide from our text file. */
+	async function getBasicWordList() {
+		console.log('Fetching Basic Word List');
+		let response = await fetch('dictionary.txt');
+		return (await response.text()).split('\n').toString();
+	}
+
+	/** When the page has loaded, unless a different word list exists in memory, grab our default word list and load it. */
+	onMount(async () => {
+		if (localStorage.getItem('wordlist') == 'empty') {
+			wordlist.set(await getBasicWordList());
+		}
+		displayWordList = "";
+		$wordlist
+			.toString()
+			.split(',')
+			.forEach((e) => {
+				displayWordList = displayWordList + e + '\n';
+			});
+	});
+
+	/** This stores the wordlist as \n seperated for display to the user. */
+	$: displayWordList = '';
+
+	function updateStoredWordlist() {
+		displayWordList = displayWordList.trim();
+		wordlist.set(displayWordList.replaceAll("\n",","));
+	}
 </script>
 
 <title>Dictionary</title>
@@ -22,10 +59,15 @@
 		This page allows you to customize the default wordlist we use or replace it with your own.
 	</P>
 	<div class="flex flex-row justify-center">
-		<Textarea {...wordlistprops} class="max-w-64" />
-		<div class="flex flex-col max-h-72">
-			<Fileupload {...fileuploadprops} class="ml-3 max-h-10 max-w-64 mb-3" />
-			<Button outline color="green" class="max-w-64 ml-3">Save Changes</Button>
+		<Textarea
+			{...wordlistprops}
+			class="max-w-64"
+			bind:value={displayWordList}
+			style="resize:none"
+		/>
+		<div class="flex max-h-72 flex-col">
+			<Fileupload {...fileuploadprops} class="mb-3 ml-3 max-h-10 max-w-64" />
+			<Button outline color="green" class="ml-3 max-w-64" on:click={updateStoredWordlist}>Save Changes</Button>
 		</div>
 	</div>
 </div>
