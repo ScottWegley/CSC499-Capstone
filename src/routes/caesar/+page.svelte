1060aedb0379d395dc2d4c5cf5499840b0f39af9
scottwegley <scottwegley@gmail.com>
2024-02-28 00:36:29 -0800

    encryption, decryption for manual caesar cipher

--- src/routes/caesar/+page.svelte
+++ src/routes/caesar/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
-	import { Button, ButtonGroup, Heading, Listgroup, ListgroupItem, P, Range} from 'flowbite-svelte';
+	import {
+		Button,
+		ButtonGroup,
+		GradientButton,
+		Heading,
+		Listgroup,
+		ListgroupItem,
+		P,
+		Range,
+		Textarea,
+		Tooltip
+	} from 'flowbite-svelte';
 	import VerticalStackButton from '$lib/components/VerticalStackButton.svelte';
+
+	/** The default standard alphabet. */
 	const DEFAULT_ALPHABET = [
 		'A',
 		'B',
@@ -53,13 +66,49 @@
 		}
 		return shiftedAlphabet;
 	}
+
+	/** This function will either encrypt or decrypt our input text based on the current encryption mode. */
+	function performOperation() {
+		inputText = inputText.toUpperCase();
+		outputText = encryptionMode
+			? caesarEncryption(inputText, currentShift)
+			: caesarDecryption(inputText, currentShift);
+	}
+
+	/** This function encrypts a given input with a Caesar Cipher of a specified shift. */
+	function caesarEncryption(input: string, shift: number) {
+		let shiftedAlphabet = getCipherAlphabet(shift);
+		let output = "";
+		for (let i = 0; i < input.length; i++) {
+			let newChar = '';
+			if(DEFAULT_ALPHABET.includes(input.charAt(i))){
+				newChar = shiftedAlphabet[DEFAULT_ALPHABET.indexOf(input.charAt(i))];
+			}
+			output = output + (newChar == '' ? input.charAt(i) : newChar);
+		}
+		return output;
+	}
+
+	/** This function encrypts a given output with a Caesar Cipher of a specified shift. */
+	function caesarDecryption(input: string, shift: number) {
+		let shiftedAlphabet = getCipherAlphabet(shift);
+		let output = "";
+		for (let i = 0; i < input.length; i++) {
+			let newChar = '';
+			if(DEFAULT_ALPHABET.includes(input.charAt(i))){
+				newChar = DEFAULT_ALPHABET[shiftedAlphabet.indexOf(input.charAt(i))];
+			}
+			output = output + (newChar == '' ? input.charAt(i) : newChar);
+		}
+		return output;
+	}
 </script>
 
 <title>Home</title>
 
 <div class="text-center">
 	<Heading tag="h4" class="mb-4">Caesar Cipher</Heading>
-	<P class="mb-2 text-md dark:text-gray-400 sm:px-16" align="center" size="sm">
+	<P class="text-md mb-2 dark:text-gray-400 sm:px-16" align="center" size="sm">
 		The Caesar Cipher is one of the oldest and most famous substitution ciphers. It works very
 		simply, only requiring knowledge of the order of the alphabet. You replace every letter in a
 		word with a letter a set distance away from it. For example, the word "Barnacle" becomes
@@ -101,11 +150,12 @@
 					>Manual {encryptionMode ? 'Encryption' : 'Decryption'} using the shift you specified.
 				</GradientButton>
 				<Tooltip>Click to change to {!encryptionMode ? 'Encryption' : 'Decryption'}.</Tooltip>
-				<GradientButton color="greenToBlue" class="mt-1.5">Run</GradientButton>
+				<GradientButton color="greenToBlue" class="mt-1.5" on:click={performOperation}
+					>Run</GradientButton
+				>
 				<Textarea placeholder="Output text" rows="4" class="mt-3" disabled bind:value={outputText}
 				></Textarea>
 			</div>
-			
 		</div>
 	</div>
 </div>
