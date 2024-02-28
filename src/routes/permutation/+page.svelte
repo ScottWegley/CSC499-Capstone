faa29749e628904e48240632d79aff58217a029a
scottwegley <scottwegley@gmail.com>
2024-02-28 02:20:00 -0800

    duplicated en/decrypt area from caesar

--- src/routes/permutation/+page.svelte
+++ src/routes/permutation/+page.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
+	import {
+		Button,
+		ButtonGroup,
+		GradientButton,
+		Heading,
+		Input,
+		Listgroup,
+		ListgroupItem,
+		P,
+		Range,
+		Textarea,
+		Tooltip
+	} from 'flowbite-svelte';
+	import VerticalStackButton from '$lib/components/VerticalStackButton.svelte';
+	import VerticalInput from '$lib/components/VerticalInput.svelte';
+	import { DEFAULT_ALPHABET } from '$lib/scripts/caesar';
+
+	/** This stores the permutated alphabet. */
+	let premutationAlphabet = DEFAULT_ALPHABET.map((l) => l);
+
+	/** Stores the text the user gives us. */
+	let inputText = '';
+
+	/** Stores the text we give back to the user. */
+	let outputText = '';
+
+	/** A boolean to represent whether we are currently in encryption mode.  If false, we are in decryption mode. */
+	let encryptionMode = true;
 </script>
 
 <title>Home</title>
@@ -12,6 +40,33 @@
 		four hundred and three septillion. A circle that many meters wide is comparable to the size of
 		the observable universe.
 	</P>
+	<div class="flex flex-col justify-center">
+		<div class="flex flex-row justify-center">
+			<div class="ml-3 flex min-w-[25%] flex-col">
+				<Textarea
+					placeholder="Input text"
+					rows="4"
+					bind:value={inputText}
+					on:change={() => {
+						inputText = inputText.toUpperCase();
+					}}
+				></Textarea>
+				<GradientButton
+					outline
+					color="greenToBlue"
+					class="mt-3"
+					on:click={() => {
+						encryptionMode = !encryptionMode;
+					}}
+					>Manual {encryptionMode ? 'Encryption' : 'Decryption'}
+				</GradientButton>
+				<Tooltip>Click to change to {!encryptionMode ? 'Encryption' : 'Decryption'}.</Tooltip>
+				<GradientButton color="greenToBlue" class="mt-1.5">Run</GradientButton>
+				<Textarea placeholder="Output text" rows="4" class="mt-3" disabled bind:value={outputText}
+				></Textarea>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
