d7d2ffcb0cf9ecd94a11dbec0d5cb0ecbb23b619
scottwegley <scottwegley@gmail.com>
2024-02-28 02:19:01 -0800

    empty permutation page w/ descriptive text

---
+++ src/routes/permutation/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<title>Home</title>
+
+<div class="text-center">
+	<Heading tag="h4" class="mb-4">Permutation Cipher</Heading>
+	<P class="text-md mb-2 dark:text-gray-400 sm:px-16" align="center" size="sm">
+		A permutation cipher is a twist on the classic Caesar cipher. Instead of translating between two
+		alphabets with one shifted from the other, your second alphabet is randomly arranged. This
+		increases the number of possible alphabets from 26 to 403291461100000000000000000, or roughly
+		four hundred and three septillion. A circle that many meters wide is comparable to the size of
+		the observable universe.
+	</P>
+</div>
+
+<style>
+</style>
