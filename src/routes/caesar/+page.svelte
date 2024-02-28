fa54cc445ad497c3edcc8b93660f148fe63d7204
scottwegley <scottwegley@gmail.com>
2024-02-27 23:57:12 -0800

    updated description text

--- src/routes/caesar/+page.svelte
+++ src/routes/caesar/+page.svelte
@@ -50,8 +50,8 @@
 	<Heading tag="h4" class="mb-4">Caesar Cipher</Heading>
 	<P class="mb-2 text-md dark:text-gray-400 sm:px-16" align="center" size="sm">
 		The Caesar Cipher is one of the oldest and most famous substitution ciphers. It works very
-		simply, only requiring knowledge of the order of the alphabet. For example, the word "Barnacle"
-		becomes "Cbsobdmf" after a 1-Right Shift. It becomes "Azqmzbkd" after a 1-Left Shift.
+		simply, only requiring knowledge of the order of the alphabet. You replace every letter in a word with a letter a set distance away from it.
+		For example, the word "Barnacle" becomes "Cbsobdmf" after a 1-Right Shift or "Azqmzbkd" after a 1-Left Shift.
 	</P>
 	<div class="flex flex-col justify-center">
 		<div class="mb-1.5">
@@ -71,6 +71,7 @@
 					{/each}
 				</div>
 			</div>
+			
 		</div>
 	</div>
 </div>
