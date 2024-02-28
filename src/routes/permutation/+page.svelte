296b8fd9b5638512f1ab2fdd027fd48c20a84114
scottwegley <scottwegley@gmail.com>
2024-02-28 03:04:11 -0800

    switched universe scale to scientific notation

--- src/routes/permutation/+page.svelte
+++ src/routes/permutation/+page.svelte
@@ -38,7 +38,7 @@
 	<P class="text-md mb-2 dark:text-gray-400 sm:px-16" align="center" size="sm">
 		A permutation cipher is a twist on the classic Caesar cipher. Instead of translating between two
 		alphabets with one shifted from the other, your second alphabet is randomly arranged. This
-		increases the number of possible alphabets from 26 to 403291461100000000000000000, or roughly
+		increases the number of possible alphabets from 26 to 4.032914611*10^26, or roughly
 		four hundred and three septillion. A circle that many meters wide is comparable to the size of
 		the observable universe.
 	</P>
