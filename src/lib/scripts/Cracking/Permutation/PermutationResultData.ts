4e03b52c7a11f3ec544733855c8e6313b9bdea34
scottwegley <scottwegley@gmail.com>
2024-03-22 01:58:14 -0700

    basic class declaration with constructor

---
+++ src/lib/scripts/Cracking/Permutation/PermutationResultData.ts
@@ -0,0 +1,18 @@
+import { ResultData } from '../ResultData';
+
+export class PermutationResultData extends ResultData {
+	/** Stores all the alphabets corresponding to our results. */
+    protected alphabets: string[][];
+
+    public constructor(
+		resultSet: string[],
+		accuracySet: number[],
+		alphabetArray: string[][],
+		accuracyThreshold: number,
+		returnPercentage: number,
+		ascendingOrder: boolean
+	) {
+		super(resultSet, accuracySet, accuracyThreshold, returnPercentage, ascendingOrder);
+		this.alphabets = alphabetArray;
+	}
+}
