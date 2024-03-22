b8be1b63a570f531551cd4b3d0cc67ae560cff61
scottwegley <scottwegley@gmail.com>
2024-03-22 01:54:52 -0700

    part of refactor

--- src/lib/scripts/Cracking/PermutationCrack.ts
+++
@@ -1,9 +0,0 @@
-import { DEFAULT_ALPHABET } from '../Ciphers/CaesarCipher';
-import { CipherCracker } from './CipherCrack';
-import { checkAccuracy } from '../Dictionary';
-import { ResultData } from './ResultData';
-
-/** Class to bruteforce crack a Caesar Cipher encrypted piece of text. */
-export class PermutationCrack extends CipherCracker { 
-    
-}
