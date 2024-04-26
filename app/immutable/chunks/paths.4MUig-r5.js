13077037b295efcf325b3b52e024035de59f091a
scottwegley <scottwegley@gmail.com>
2024-04-26 14:53:53 -0700

    Updates

---
+++ app/immutable/chunks/paths.4MUig-r5.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1tt30j5)==null?void 0:s.base)??"/CSC499-Capstone";var t;const e=((t=globalThis.__sveltekit_1tt30j5)==null?void 0:t.assets)??a;export{e as a,a as b};
